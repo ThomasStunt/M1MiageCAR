bb8df61d6b26c5edbcce5d31372d1dde3952ea85
ThomasStunt <thomas.l.perrier@gmail.com>
2018-01-27 13:46:45 +0100

    Add files via upload
    final changes

--- TPCalculette/src/CalculServeur.java
+++ TPCalculette/src/CalculServeur.java
@@ -4,34 +4,70 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
 
+/**
+ * Calculator Server
+ * Accept connections on the port 4081 and computes the result of the operations he is given.
+ */
 public class CalculServeur {
 
 	public static void main(String[] args) {
+		System.out.println("CalculServer launch...");
 		ServerSocket ps = null;
+		Socket as = null;
+		BufferedReader in = null;
+		DataOutputStream out = null;
+		Boolean exit = false;
 		try {
-		ps = new ServerSocket(4000);
-			while (true) {
-				Socket as = ps.accept();
+			ps = new ServerSocket(4081);
+			while (!exit) {
+				as = ps.accept();
 				System.out.println("Connection established");
-				BufferedReader in = new BufferedReader(
-				new InputStreamReader(as.getInputStream()));
-				DataOutputStream out = new DataOutputStream(as.getOutputStream());
-				String msg = in.readLine();
-				while(!msg.contentEquals("stop")) {
-					System.out.println("Message received: " + msg);
-					String resp = String.valueOf(ExpressionParser.eval(msg));
-					out.writeBytes(resp+'\n');
-					System.out.println("Response has been sent.");
-					msg = in.readLine();
-					if(msg.contentEquals("stop")) {
-						System.out.println("Connection over");
+				in = new BufferedReader(new InputStreamReader(as.getInputStream()));
+				out = new DataOutputStream(as.getOutputStream());
+				try {
+					String msg = in.readLine();
+					while (!exit) {
+						try {
+							System.out.println("Message received : " + msg);
+							String resp = null;
+							if (msg.contentEquals("exit")) {
+								exit = true;
+								System.out.println("Connection over");
+							} else if (msg.contentEquals("leave")) {
+								System.out.println("Client leave...");
+								break;
+							} else {
+								resp = String.valueOf(ExpressionParser.eval(msg));
+								out.writeBytes(resp + '\n');
+								System.out.println("Response \"" + resp + "\" has been sent.");
+								msg = in.readLine();
+							}
+						} catch (RuntimeException e) {
+							System.err.println("Can't handle this calcul...");
+							out.writeBytes("Error\n");
+							System.err.println("Error has been sent.");
+							msg = in.readLine();
+						}
 					}
+				} catch (SocketException e) {
+					System.out.println("Client leave...");
 				}
 			}
 		} catch (IOException ex) {
+			System.err.println("An error occured, CalculServer will shut down...");
+			ex.printStackTrace();
+		} finally {
+			try {
+				ps.close();
+				as.close();
+				in.close();
+				out.close();
+			} catch (IOException e) {
+			}
 			System.exit(-1);
 		}
 	}
-	
+
 }
