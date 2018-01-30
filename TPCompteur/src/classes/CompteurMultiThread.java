549ea5bb1a932ecb1383a9a10b6861f248570ea0
ThomasStunt <perrier@a16p6.fil.univ-lille1.fr>
2018-01-30 12:01:47 +0100

    lé freds cé bi1

--- TPCompteur/src/classes/CompteurMultiThread.java
+++ TPCompteur/src/classes/CompteurMultiThread.java
@@ -1,11 +1,60 @@
 package classes;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
 public class CompteurMultiThread {
 
 	private int nThread;
+	private List <CountingThread> threads;
+	private File inputFile;
+	private int nbLinesToRead;
+	private int nbLinesLeft;
 	
-	public CompteurMultiThread(int nThread) {
+	public CompteurMultiThread(int nThread, File f) throws IOException {
 		this.nThread = nThread;
+		this.inputFile = f;
+		int lines = countLines();
+		nbLinesToRead = lines / nThread;
+		nbLinesLeft = lines % nThread;
 	}
 	
+	public int countLines() throws IOException {
+	    InputStream is = new BufferedInputStream(new FileInputStream(inputFile));
+	    try {
+	        byte[] c = new byte[1024];
+	        int count = 0;
+	        int readChars = 0;
+	        boolean empty = true;
+	        while ((readChars = is.read(c)) != -1) {
+	            empty = false;
+	            for (int i = 0; i < readChars; ++i) {
+	                if (c[i] == '\n') {
+	                    ++count;
+	                }
+	            }
+	        }
+	        return (count == 0 && !empty) ? 1 : count;
+	    } finally {
+	        is.close();
+	    }
+	}
+	
+	public static void main(String[] args) {
+		CompteurMultiThread cmt;
+		try {
+			try {
+				cmt = new CompteurMultiThread(Integer.parseInt(args[0]), null);
+			} catch (ArrayIndexOutOfBoundsException | NumberFormatException e) {
+				System.out.println("Cannot read value, default is 4");
+				cmt = new CompteurMultiThread(4, null);
+			}
+		} catch (IOException e) {
+			System.err.println(e.getMessage());
+		}
+	}
 }
--- TPCompteur/src/classes/CountingThread.java
+++ TPCompteur/src/classes/CountingThread.java
@@ -1,15 +1,37 @@
 package classes;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
 public class CountingThread extends Thread {
 
+	private int firstLine, lastLine;
+	private File inputFile;
 	private CompteurSeq cs;
 	
-	public CountingThread(String s) {
-		cs = new CompteurSeq(s);
+	public CountingThread(int fLine, int lLine, File iFile) throws IOException {
+		this.firstLine = fLine;
+		this.lastLine = lLine;
+		this.inputFile = iFile;
+		cs = new CompteurSeq(readLines());
+	}
+	
+	public String readLines() throws IOException {
+		String res = "";
+		try (Stream<String> lines = Files.lines(Paths.get(inputFile.getPath()))) {
+			for(int i = firstLine; i <= lastLine; i++) {
+		    	String line = lines.skip(i - 1).findFirst().get();
+		    	res = res.concat(line);
+			}
+		}
+		return res;
 	}
 	
 	@Override
-	public void run() {
+	public void run(){
 		super.run();
 	}
 	
