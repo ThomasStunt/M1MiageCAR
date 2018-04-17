ed201dcc1845fa5f248865834f8f7ea70cde6c57
ThomasStunt <perrier@a16p8.fil.univ-lille1.fr>
2018-04-17 12:05:23 +0200

    sq

--- MiageBook/src/main/java/DisconnectServlet.java
+++ MiageBook/src/main/java/DisconnectServlet.java
@@ -2,22 +2,28 @@
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 public class DisconnectServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    HttpSession userSession;
+	Cookie[] cookies;
+	Cookie cook;
 	
 	@Override
     protected void doGet(HttpServletRequest reqest, HttpServletResponse response) 
         throws ServletException, IOException {
 		
-		userSession = reqest.getSession(false);
-		userSession.invalidate();
+		cookies = reqest.getCookies();
+		for(int i = 0; i<cookies.length; i++) {
+			cook = cookies[i];
+			if(cook.getName().contentEquals("psd")) {
+				cook.setMaxAge(0);
+			}
+		}
 		response.sendRedirect("localhost:8080/SimpleServlet/home.jsp");
 		
 		this.destroy();
